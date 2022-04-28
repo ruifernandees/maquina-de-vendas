0126d326b9999f2db19f6e90fa54c9f239c8e2a9
ruifernandees <dev.ruifernandes@gmail.com>
2022-04-28 11:06:44 -0300

    feat: q0 grayscale

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -7,11 +7,15 @@ public class ItemManager : MonoBehaviour
 {
     public Animator[] animators;
     public GameObject[] arrowes;
+    public SpriteRenderer initialState;
+    public Sprite initialStateNormalColor;
+    public Sprite initialStateGrayscale;
     private int currentAnimator = -1;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.initialState.sprite = this.initialStateGrayscale; 
     }
 
     // Update is called once per frame
@@ -24,12 +28,14 @@ public class ItemManager : MonoBehaviour
                 }
                 this.animators[currentAnimator].SetBool("canClear", false);
                 this.currentAnimator = -1;
+                this.initialState.sprite = this.initialStateGrayscale;
             }
         }
     }
 
     public void moveItemDown(int itemIndex) {
         if (this.currentAnimator != -1) return;
+        this.initialState.sprite = this.initialStateNormalColor;
         this.animators[itemIndex].SetBool("canMoveDown", true);
         this.arrowes[itemIndex].SetActive(true);
         this.currentAnimator = itemIndex; 
